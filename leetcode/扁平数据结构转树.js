82e36e284b2e847f69e24a453d6bffc71ed1f556
leaf <821976950@qq.com>
2021-07-21 21:33:24 +0800

    fix: 更新

--- leetcode/扁平数据结构转树.js
+++ leetcode/扁平数据结构转树.js
@@ -1,42 +1,41 @@
 function toTree(arr) {
-    arr = arr || [];
-    // 查找根节点
-    var rootTree = null;
-    var other = arr.filter(function (tree) {
-        if (tree.pid === 0)
-            rootTree = tree;
-        return tree.pid !== 0;
-    });
-    // 根据根节点找子节点
-    return getChildren(other, rootTree);
+  arr = arr || [];
+  // 查找根节点
+  let rootTree = null;
+  const other = arr.filter(function (tree) {
+    if (tree.pid === 0)
+      rootTree = tree;
+    return tree.pid !== 0;
+  });
+  // 根据根节点找子节点
+  return getChildren(other, rootTree);
 }
-function getChildren(origin, currentTree) {
-    if (origin === void 0) { origin = []; }
-    var children = null;
-    var other = [];
-    children = origin.filter(function (leaf) {
-        if (leaf.pid === currentTree.id) {
-            return true;
-        }
-        else {
-            other.push(leaf);
-            return false;
-        }
-    });
-    var length = children.length;
-    while (children.length) {
-        for (var i = 0; i <= length; i++) {
-            getChildren(other, children[i]);
-        }
-        length--;
+
+function getChildren(origin, currentNode) {
+  const otherArr = [];
+  const currentArr = origin.filter(tree => {
+    if (tree.pid === currentNode.id) {
+      return true;
+    } else {
+      otherArr.push(tree);
+      return false;
     }
-    currentTree.children = children;
-    return currentTree;
+  });
+  currentNode.children = currentArr;
+  let length = currentArr.length;
+  while (length) {
+    for (let i = 0; i < length; i++) {
+      getChildren(otherArr, currentArr[i]);
+    }
+    length--;
+  }
+  return currentNode
 }
-toTree([
-    { id: 1, name: '部门1', pid: 0 },
-    { id: 2, name: '部门2', pid: 1 },
-    { id: 3, name: '部门3', pid: 1 },
-    { id: 4, name: '部门4', pid: 3 },
-    { id: 5, name: '部门5', pid: 4 },
-]);
+const result = toTree([
+  {id: 1, name: '部门1', pid: 0},
+  {id: 2, name: '部门2', pid: 1},
+  {id: 3, name: '部门3', pid: 1},
+  {id: 4, name: '部门4', pid: 3},
+  {id: 5, name: '部门5', pid: 4},
+])
+console.log(JSON.stringify(result))
